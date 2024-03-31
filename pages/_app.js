8171aa7b0b2608107cdf923ddc005adc67d953c5
ENZOMOTIVE <aayushmanbhabapadhy@gmail.com>
2024-03-31 11:14:04 +0530

    setup-done
    Next work on Components

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,19 @@
 import "@/styles/globals.css";
 
+
+//Internal import
+import { TrackingProvider } from "@/Context/Tracking"; 
+import { NavBar , Footer} from "@/Components";
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+    <TrackingProvider>
+    <NavBar/>
+      <Component {...pageProps}/>
+    </TrackingProvider>
+    <Footer/>
+    </>
+  )
+ 
 }
